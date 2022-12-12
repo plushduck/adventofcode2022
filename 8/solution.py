24a698d13e222c973485ab20f6593138f3ba479a
phillipalexander <phillipalexander@granular.ag>
2022-12-12 14:40:48 -0600

    Day 7 part 2

--- 8/solution.py
+++ 8/solution.py
@@ -5,6 +5,7 @@ class tree():
         self.blocked_right = False
         self.blocked_up = False
         self.blocked_down = False
+        self.scenic_score = 1
 
     def blocked(self):
         return self.blocked_left and self.blocked_right and self.blocked_up and self.blocked_down
@@ -58,6 +59,57 @@ def part_1():
     print(nvisible)
 
 
+def part_2():
+    nrows = len(FOREST)
+    ncols = len(FOREST[0])
+    for r in range(0,nrows):
+        # last blocker at a given height
+        visibility = [1]*10
+        FOREST[r][0].scenic_score = 0
+        FOREST[r][ncols-1].scenic_score = 0
+        for c in range(1,ncols):
+            tree = FOREST[r][c]
+            height = tree.height
+            tree.scenic_score *= visibility[height]
+            for i in range(0,height+1):
+                visibility[i] = 1
+            for i in range(height+1, 10):
+                visibility[i] += 1
+        visibility = [1]*10
+        for c in range(ncols-2,-1,-1):
+            tree = FOREST[r][c]
+            height = tree.height
+            tree.scenic_score *= visibility[height]
+            for i in range(0,height+1):
+                visibility[i] = 1
+            for i in range(height+1, 10):
+                visibility[i] += 1
+    for c in range(0,ncols):
+        visibility = [1]*10
+        FOREST[0][c].scenic_score = 0
+        FOREST[nrows-1][c].scenic_score = 0
+        for r in range (1,nrows):
+            tree = FOREST[r][c]
+            height = tree.height
+            tree.scenic_score *= visibility[height]
+            for i in range(0,height+1):
+                visibility[i] = 1
+            for i in range(height+1, 10):
+                visibility[i] += 1
+        visibility = [1]*10
+        for r in range(nrows-2,-1,-1):
+            tree = FOREST[r][c]
+            height = tree.height
+            tree.scenic_score *= visibility[height]
+            for i in range(0,height+1):
+                visibility[i] = 1
+            for i in range(height+1, 10):
+                visibility[i] += 1
+
+    max_score = max([max([t.scenic_score for t in row]) for row in FOREST])
+    print(max_score)
+
+
 if __name__ == "__main__":
-    part_1()
+    part_2()
 
